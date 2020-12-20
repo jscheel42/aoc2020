9027d2c85587d138e10e9ec4cbe76fff4ff0cb73
Joshua Scheel <jscheel42@gmail.com>
2020-12-20 16:06:14 -0700

    first commit aoc_util

---
+++ aoc_util/src/lib.rs
@@ -0,0 +1,24 @@
+use std::fs::File;
+use std::vec::Vec;
+use std::io::{self, prelude::*, BufReader}; // https://stackoverflow.com/questions/45882329/read-large-files-line-by-line-in-rust
+
+pub fn read_file_to_vec<T: std::str::FromStr>(filename: String) -> io::Result<Vec<T>> where <T as std::str::FromStr>::Err: std::fmt::Debug {
+    let file = File::open(filename)?;
+    let reader = BufReader::new(file);
+
+    let mut vec: Vec<T> = Vec::new();
+    for line_str in reader.lines() {
+        let line: T = line_str?.parse().unwrap();
+        vec.push(line);
+    }
+
+    Ok(vec)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
