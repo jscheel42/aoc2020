ec8985a08073f18ccd99afa388edbf9fe57b782f
Joshua Scheel <jscheel42@gmail.com>
2020-12-20 16:30:54 -0700

    use reference input for read_file_to_vec

--- aoc_util/src/lib.rs
+++ aoc_util/src/lib.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::vec::Vec;
 use std::io::{self, prelude::*, BufReader}; // https://stackoverflow.com/questions/45882329/read-large-files-line-by-line-in-rust
 
-pub fn read_file_to_vec<T: std::str::FromStr>(filename: String) -> io::Result<Vec<T>> where <T as std::str::FromStr>::Err: std::fmt::Debug {
+pub fn read_file_to_vec<T: std::str::FromStr>(filename: &String) -> io::Result<Vec<T>> where <T as std::str::FromStr>::Err: std::fmt::Debug {
     let file = File::open(filename)?;
     let reader = BufReader::new(file);
 
