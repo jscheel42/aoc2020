1e73e3612b0c0c00668620fdb4c588d7981f509c
Joshua Scheel <jscheel42@gmail.com>
2020-12-20 20:43:28 -0700

    add day2-2

---
+++ day2-2/src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use aoc_util::read_file_to_vec;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let file_content: Vec<String> = read_file_to_vec(filename).unwrap();
+    let mut correct_password_count: usize = 0;
+    for line in file_content {
+        // Parse out the different values we need from the input
+        let mut iter = line.split_whitespace();
+
+        let count_range: String = iter.next().unwrap().to_string();
+        let mut count_iter = count_range.split("-");
+        let idx_1: usize = count_iter.next().unwrap().parse::<usize>().unwrap() - 1;
+        let idx_2: usize = count_iter.next().unwrap().parse::<usize>().unwrap() - 1;
+
+        let target_char: String = iter.next().unwrap().to_string().replace(":", "");
+
+        let password: String = iter.next().unwrap().to_string();
+
+        // Compare the password to the criteria        
+        let mut match_count = 0;
+        if password.chars().nth(idx_1).unwrap().to_string() == target_char {
+            match_count += 1;
+        }
+        if password.chars().nth(idx_2).unwrap().to_string() == target_char {
+            match_count += 1;
+        }
+        if match_count == 1 {
+            correct_password_count += 1;
+        }
+
+    }
+    println!("{}", correct_password_count);
+}
