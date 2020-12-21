be172b46ec6cbdbf730911a50a68be21a1f71872
Joshua Scheel <jscheel42@gmail.com>
2020-12-20 17:15:50 -0700

    day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,19 +4,37 @@ use aoc_util::read_file_to_vec;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    let (i, j) = get_nums(filename).unwrap();
+    let (i, j) = get_2_nums(filename).unwrap();
     println!("{} x {}", i, j);
     println!("{}", i * j);    
+    let (i, j, k) = get_3_nums(filename).unwrap();
+    println!("{} x {} x {}", i, j, k);
+    println!("{}", i * j * k);    
 }
 
-// fn get_nums(filename: &String) -> std::io::Result<(u64, u64)>{
-fn get_nums(filename: &String) -> Result<(u64, u64), std::fmt::Error> {
+fn get_2_nums(filename: &String) -> Result<(u64, u64), std::fmt::Error> {
     let vec: Vec<u64> = read_file_to_vec(filename).unwrap();
     
     for i in vec.iter() {
         for j in vec.iter() {
             if i + j == 2020 {
                 return Ok((*i, *j))
+            }
+        }
+    }
+
+    Err(std::fmt::Error)
+}
+
+fn get_3_nums(filename: &String) -> Result<(u64, u64, u64), std::fmt::Error> {
+    let vec: Vec<u64> = read_file_to_vec(filename).unwrap();
+    
+    for i in vec.iter() {
+        for j in vec.iter() {
+            for k in vec.iter() {
+                if i + j + k == 2020 {
+                    return Ok((*i, *j, *k))
+                }
             }
         }
     }
